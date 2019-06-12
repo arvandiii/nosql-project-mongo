4b58e5163576c78347ac13539e94c9eb96a0e39c
alirezaarv <alireza.arv.mail@gmail.com>
2019-06-12 15:14:34 +0430

    pagination

--- api/home.js
+++ api/home.js
@@ -3,21 +3,32 @@ const _ = require("underscore");
 const Question = mongo.model("Question");
 const QuestionLabel = mongo.model("QuestionLabel");
 
-const getQuestionsBasedOnLabel = async labelIds => {
+const getQuestionsBasedOnLabel = async (labelIds, skip, limit) => {
   const questionLabels = await QuestionLabel.find({
     labelId: { $in: labelIds }
   });
   const questions = await Question.find({
     _id: { $in: _.map(questionLabels, ql => ql.questionId) }
-  }).limit(5);
+  })
+    .skip(skip)
+    .limit(limit);
   return questions;
 };
 
 const answerPage = async (ctx, params) => {
   const { skillIds, interestIds } = ctx.user;
+  const { skip, limit } = params;
   return {
-    questionsBasedOnInterests: await getQuestionsBasedOnLabel(interestIds),
-    questionsBasedOnSkills: await getQuestionsBasedOnLabel(skillIds)
+    questionsBasedOnInterests: await getQuestionsBasedOnLabel(
+      interestIds,
+      skip || 5,
+      limit || 5
+    ),
+    questionsBasedOnSkills: await getQuestionsBasedOnLabel(
+      skillIds,
+      skip || 5,
+      limit || 5
+    )
   };
 };
 
